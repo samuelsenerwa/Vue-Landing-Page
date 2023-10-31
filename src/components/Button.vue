8dd0d85ae2ea06637b80b6dd654da5f396f32e02
samuelsenerwa <samuelsenerwa@gmail.com>
2023-10-31 16:31:33 +0300

    Added subscribe section

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -13,6 +13,7 @@ const props = defineProps({
     :type="type"
     v-if="btnType === 'secondary'"
     class="font-theme-heading bg-theme-secondary px-6 py-2 text-white rounded shadow-md hover:bg-white border-2 border-transparent hover:border-theme-secondary hover:text-theme-secondary cursor-pointer transition duration-200 w-full lg:w-auto mt-4 lg:mt-0 focus:outline-none"
-  ></button>
-  <slot></slot>
+  >
+    <slot></slot>
+  </button>
 </template>
